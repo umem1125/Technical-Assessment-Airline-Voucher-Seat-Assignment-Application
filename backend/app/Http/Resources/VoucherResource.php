b30d9ed35ecdfcb7fe8cb9bcecc52bfa8e17f3f0
umem1125 <chaerulumam1125@gmail.com>
2026-07-15 19:56:07 +0700

    feat: implement voucher resource

---
+++ backend/app/Http/Resources/VoucherResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VoucherResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'success' => true,
+            'seats' => [
+                $this->seat1,
+                $this->seat2,
+                $this->seat3,
+            ],
+        ];
+    }
+}
