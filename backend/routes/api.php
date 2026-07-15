5347259b435c33da0fdbfed99cb02655da5bd22c
umem1125 <chaerulumam1125@gmail.com>
2026-07-15 20:20:18 +0700

    feat: implement api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\API\VoucherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/check', [VoucherController::class, 'check']);
+Route::post('/generate', [VoucherController::class, 'generate']);
